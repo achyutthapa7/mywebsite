fe9cd3fa7e241cd03583e5a40e357e234a5ffb6e
achyutthapa7 <achyut777thapa@gmail.com>
2023-08-12 15:08:06 +0545

    again change

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Achyut Thapa</h1>
+      <h1>My Name Is Achyut Thapa</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
